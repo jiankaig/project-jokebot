4962266bd7ff27adbcfc3af884adbc229fc8a7fe
Chong Junn Teo <chong_junn_teo@aiap.sg>
2022-06-10 13:57:20 +0800

    Update streamlit.py
    update streamlit to include gif for funny not funny

--- aiap-team-7-project-jokebot/src/streamlit.py
+++ aiap-team-7-project-jokebot/src/streamlit.py
@@ -3,8 +3,11 @@ import logging
 import hydra
 import streamlit as st
 import uvicorn
-
+from PIL import Image
 import aiap_team_7_project_jokebot as jokebot
+
+GIF_FUNNY = "https://i.gifer.com/4j.gif"
+GIF_UNFUNNY = "https://c.tenor.com/LI7vXH2DTuMAAAAC/the-office-michael-scott.gif"
 
 
 @st.cache(allow_output_mutation=True)
@@ -33,6 +36,7 @@ def main(args):
     model_path = os.path.join(
         hydra.utils.get_original_cwd(), args["inference"]["modelpath"]
     )
+    imgs = os.path.join(hydra.utils.get_original_cwd(), args["inference"]["imgs"])
 
     logger.info("Loading the model...")
     pred_model = load_model(model_type, model_path)
@@ -40,21 +44,31 @@ def main(args):
     logger.info("Loading dashboard...")
     title = st.title("AIAP Team 7 Project Jokebot")
 
-    text_input = st.text_area("Review", placeholder="Insert your jokes here")
+    text_input = st.text_area("Joke", placeholder="Insert your joke here")
+    get_humour_sentiment = st.button("Get humour sentiment")
 
-    if st.button("Get humour sentiment"):
+    waiting_text = st.empty()
+    response = st.empty()
+    gif_response = st.empty()
+
+    if get_humour_sentiment:
         logger.info("Conducting inferencing on text input...")
         # TODO: curr_pred_result
-
-        sentiment = pred_model.predict(text_input)
-        # curr_pred_result = float(pred_model.predict([text_input])[0])
-        # sentiment = "positive" if curr_pred_result > 0.5 else "negative"
+        waiting_text.text("Waiting for the slow model to provide a response...")
+        humour_level = pred_model.predict(text_input)
+        waiting_text.text("")
+        humour_percent = "{:.0%}".format(humour_level)
         logger.info(
-            f"Inferencing has completed. Text input: {text_input}. Sentiment: {sentiment}"
+            f"Inferencing has completed. Text input: {text_input}. Sentiment: {humour_percent}"
         )
-        st.write(f"The sentiment of the joke is {sentiment}.")
-    else:
-        st.write("Awaiting a review...")
+        response.text(f"Your joke is {humour_percent} funny.")
+        if humour_level > 0.5:
+            markdown = f"![]({GIF_FUNNY})"
+
+        else:
+            markdown = f"![]({GIF_UNFUNNY})"
+
+        gif_response.markdown(markdown)
 
 
 if __name__ == "__main__":
