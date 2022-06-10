b2b119262e7ae07b18b059182c26dce5484e2166
Jiankai G <jiankaig@gmail.com>
2022-06-10 11:25:55 +0800

    Update deps.py
    due to failures in fapi webserver

--- aiap-team-7-project-jokebot/src/aiap_team_7_project_jokebot_fastapi/deps.py
+++ aiap-team-7-project-jokebot/src/aiap_team_7_project_jokebot_fastapi/deps.py
@@ -1,9 +1,7 @@
-import aiap_team_7_project_jokebot as jokebot
-import aiap_team_7_project_jokebot_fastapi as jokebot_fapi
-from aiap_team_7_project_jokebot.modeling import models
-
+from .. import aiap_team_7_project_jokebot as jokebot
+from .. import aiap_team_7_project_jokebot_fastapi as jokebot_fapi
 
 PRED_MODEL = jokebot.modeling.utils.load_model(
     jokebot_fapi.config.SETTINGS.PRED_MODEL_PATH)
 
-PRED_MODEL_CUSTOM = models.HumourRecognitionModel(path_to_saved_model=jokebot_fapi.config.SETTINGS.PRED_MODEL_PATH)
+PRED_MODEL_CUSTOM = jokebot.modeling.models.HumourRecognitionModel(path_to_saved_model=jokebot_fapi.config.SETTINGS.PRED_MODEL_PATH)
